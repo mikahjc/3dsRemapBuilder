804a1e229acbf1a0d4d8a52e30646f55d0b43ae7
Mikah Chapman <mikah.chapman@gmail.com>
2021-04-29 01:16:07 -0600

    angular 11 upgrade and more audit fixes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })
   ],
   exports: [RouterModule],
   providers: []
--- src/app/app-shell/circle-pad-configuration/circle-pad-configuration.component.spec.ts
+++ src/app/app-shell/circle-pad-configuration/circle-pad-configuration.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { CirclePadConfigurationComponent } from './circle-pad-configuration.component';
 
@@ -6,7 +6,7 @@ describe('ButtonMapConfigurationComponent', () => {
   let component: CirclePadConfigurationComponent;
   let fixture: ComponentFixture<CirclePadConfigurationComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ CirclePadConfigurationComponent ]
     })
