8d192fbe16f093759e1cbcd13cb5be53b1fb282f
mikahjc <mikah.chapman@gmail.com>
2021-05-12 19:37:09 -0600

    Fix for #14: this issue was handled on the BS3DS side, but I didn't implement the same logic in the rehid method

--- shared/model/coordinates.ts
+++ shared/model/coordinates.ts
@@ -14,7 +14,7 @@ export class Coordinates implements InputClass{
     }
 
     toRehid(): number[] {
-        return [this.x, this.y]
+        return [(this.x - 240), this.y]
     }
 
     toTsData(): number {
